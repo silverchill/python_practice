d62fa07ad7e0b483f4f0b8c0310c6228a40282e7
Steve <ustinsteve@gmail.com>
2023-09-26 17:56:24 +0100

    successful worked on data type

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+x = "Hello World"
+print(type(x)) #To get the type of data use type()
+
+x = 1j
+print(type(x)) #complex data type
+
+x = ("Steve", "Solo", "Chi")
+print(type(x))     #tuple data type
+
+x = ["Steve", "Solo", "Chi"]
+print(type(x))    #list data type
+
+x = range(7)
+print(type(x))      #range data type
+
+x = {"apple", "banana", "cherry"}
+print(type(x))      #set data type
+
+x = {"apple" : "banana", "cherry" : 50}
+print(type(x))      #dict data type
+
+x = b"Steve"
+print(type(x))      #bytes data type
+
+x = False
+print(type(x)) #bool data type NB: T or F should be in capital
