40e0daa47789528930135418a1eb1559a81ae6a2
Steve <ustinsteve@gmail.com>
2023-09-26 16:32:39 +0100

    worked on indent, casting, separate outputs with commas etc)

---
+++ myfile.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+if 5 > 2:
+    print("Five is greater than 2!") #indentation with at least a space
+x = 5  
+x = "Hello, World!"
+print(x)
+#casting
+r = str(3)
+s = int(3)
+t = float(3)
+print(r)
+print(s)
+print(t)
+a, b, c = "Obi ", "Ada ", "Ede"
+print(a + b + c) # Add extra space at the end of each string to be able to get a space to divide them
+h = 5
+i = 6
+print(h + i)
+h = 5
+i = 6
+print(5, 6) # Separate with commas to print multiple output
