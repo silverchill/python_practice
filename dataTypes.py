c5a6ae4142e298c5e8cafe89309c569404a643cd
Steve <ustinsteve@gmail.com>
2023-09-27 14:07:52 +0100

    Worked on modification and concatenation of strings

--- dataTypes.py
+++ dataTypes.py
@@ -24,3 +24,43 @@ print(type(x))      #bytes data type
 
 x = False
 print(type(x)) #bool data type NB: T or F should be in capital
+
+b = """This a way to assign,
+long string like a sentence"""
+print(b)
+
+for w in "Steve":
+    print(w)        #for loop, To print each character in a way
+
+e = "Hello, Stephen"
+print(len(e))       #To count the no of character in a string(return length of string)
+
+cat = "We are good together"
+print("hat" not in cat) #To check if a word is in a sentence, we use 'in or not' to get true or false
+
+txt = "The best things in life are free!"
+if "freed" in txt:
+  print("Yes, 'freed' is present.")
+else:
+    print("No, 'freed' is absent.") #Using if and else statement
+
+r = "Stephen, Egwuatu"
+print(r[2:6])   #Get the characters from position 2 to position 6(not included)
+
+y = "Stephen, Egwuatu"
+print(y[:6])    #Get the characters from the start to position 6(not included)
+
+z = "Stephen, Egwuatu"
+print(z[3:]) #Get the characters from position 2, and all the way to the end
+
+c = "stella"
+print(c.upper()) #To return a string in upper case use upper()
+
+f = "steve, "
+print(f.strip()) #To return string without the whitespace
+
+d = "Hello, World!"
+print(d.replace("Hello", "Ste")) #To replace a string or character
+
+g = "Obi is a boy!"
+print(g.split(" ")) #To return another string
