26d01966f380088f1d2496ca6163c7a80f0855b7
Steve <ustinsteve@gmail.com>
2023-09-28 15:31:53 +0100

    Worked on change list item

---
+++ list.py
@@ -0,0 +1,8 @@
+value = ["Apple", "Egg", "Castle"]
+value[1] = "bag"
+print(value)       #To change the value of a specific item, refer to the index no
+
+g = ["Water", "Bread", "Dog", "Cow"]
+g.insert(3, "cat")
+print(g)        #To "insert" cat as the fourth item in the list
+
