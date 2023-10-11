164501c8d21bd4b03136e8eecda8d01406094304
Steve <ustinsteve@gmail.com>
2023-10-11 15:57:01 +0100

    completed tuple and its exercises

--- tuple.py
+++ tuple.py
@@ -2,18 +2,13 @@
 Tuples are used to store multiple items in a single variable, A tuple is a collection which is ordered and
 unchangeable. Tuples are written with round brackets
 """
-name = "Alice"
-age = 30
-print("Name:", name)
-print("Age:", age)
 
-name = "Charlie"
-age = 35
-good= f"My name is {name} and I am {age} years old."
-print(good)
+thistuple = ("apple", "banana", "cherry") # This is a tuple
+print(thistuple)
 
-name = "Eric"
-profession = "comedian"
-affiliation = "Monty Python"
-message = f"Hi {name}. You are a {profession}. You were in {affiliation}."
-print(message)
+thistuple = tuple(("apple", "banana", "cherry")) # This is also a tuple
+print(thistuple)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(*red,) = fruits
+print(red) # adding * to the variable name will make the rest values come as a list
